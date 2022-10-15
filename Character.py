92505f4b6323801fab62f062b69cf539257ff0c7
SiYeungLee@TIP209 <LeeSiYeoung.h4801y@naver.com>
2022-10-16 04:33:36 +0900

    에너미 구현, 플레이어와 적의 물리 충돌시 플레이어의 히트박스 통해 데미지 구현, 좌우 바라보게 스프라이트 변경

--- Character.py
+++ Character.py
@@ -1,17 +1,20 @@
 from pico2d import *
-
 # 캐릭터가 가져야 할것
 class Player:
     image = None
+    type = None
     frame = 0
     state = "IDLE"
+
     x = 1280//2
     y = 720//2
+    width = 40
+    height = 40
 
     x_dir = 0
     y_dir = 0
 
-    speed = 1.2  # 이동속도
+    speed = 2  # 이동속도
 
     MaxHp = 100.0  # 최대 Hp
     Hp = MaxHp  # 현재 HP
@@ -32,24 +35,31 @@ class Player:
     Magent = 0.0  # 경험치 흡수 범위
     Revival = 0  # 부활 횟수
 
-    invisivleTime = 0.0  # 최대 무적시간
-    Max_invisivleTime = 0.0  # 총 무적시간
+    invisivleTime = 0.0  # 무적시간
+    Max_invisivleTime = 1.0  # 최대 무적시간
 
     invers = False # 캐릭터 오른쪽, 왼쪽
-
+    def check_Enemy_Coll(self, enemy_left, enemy_right, enemy_top, enemy_bottom, enemy_Attack):
+        if self.x - self.width//2 < enemy_right and self.y - self.height//2 < enemy_bottom and self.x + self.width//2 > enemy_left and self.y + self.height//2 > enemy_top:
+            if self.invisivleTime <= 0:
+                print("hit")
+                self.Hp -= enemy_Attack
+                self.invisivleTime = self.Max_invisivleTime
+        pass
     def draw(self):
-        if self.invers != True:
             if self.state == "IDLE":
                 self.frame = self.frame % 2 + 1
                 if not self.invers:
-                    self.image.clip_draw(0 + self.frame * 34, 616 - 80, 34, 33, self.x, self.y)
+                    self.image.clip_draw(0 + self.frame * 34, 616 - 80, 34, 33, self.x, self.y, self.width,self.height)
                 else :
-                    self.image.clip_draw(34 * self.frame - self.frame * 34, 616 - 80, 34, 33, self.x, self.y)
+                    self.image.clip_draw(0 + self.frame * 34, 616 - 40, 34, 33, self.x, self.y, self.width,self.height)
 
             elif self.state == "RUN":
-                self.frame = self.frame % 7 + 1
-                self.image.clip_draw(112 + self.frame * 34, 616 - 80, 34, 33, self.x, self.y)
-        pass
+                self.frame = self.frame % 6 + 1
+                if not self.invers:
+                    self.image.clip_draw(114 + self.frame * 33, 616 - 80, 33, 33, self.x, self.y, self.width,self.height)
+                else:
+                    self.image.clip_draw(114 + self.frame * 34, 616 - 40, 34, 33, self.x, self.y, self.width,self.height)
 
     def handle_events(self, events):
         for event in events:
@@ -58,10 +68,10 @@ class Player:
                 self.state = "RUN"
                 if event.key == SDLK_RIGHT:
                     self.x_dir += 1
-                    inverse = False
+                    self.invers = True
                 elif event.key == SDLK_LEFT:
                     self.x_dir -= 1
-                    self.inverse = True
+                    self.invers = False
                 elif event.key == SDLK_UP:
                     self.y_dir += 1
                 elif event.key == SDLK_DOWN:
@@ -81,6 +91,15 @@ class Player:
                 self.state = "IDLE"
             pass
 
+
+    def ExpMagnet(self):
+        pass
+    def levelUP(self):
+        if self.Exp >= self.MaxExp:
+            self.Exp = 0
+            self.MaxExp = self.MaxHp * 1.5
+            self.Level = self.Level + 1
+        pass
     def Move(self, MapEndLeft = 0, MapEndRight = 1280, MapEndBottom = 720, MapEndTop = 0):
         if self.x > MapEndLeft and self.x < MapEndRight:
             self.x += self.x_dir * self.speed
@@ -100,60 +119,124 @@ class Player:
 
         pass
 
-class Enemy:
-    name = None
-    image = None
-    frame = 0
-    state = None
 
-    x = 0
-    y = 0
+class Partner:
+    image = None
+    type = None
+    frame = 0
+    state = "IDLE"
+
+    x = 1280 // 2
+    y = 720 // 2
+    width = 40
+    height = 40
 
     x_dir = 0
     y_dir = 0
 
-    speed = 1.0  # 이동속도
+    speed = 2  # 이동속도
 
-    MaxHp = 10.0  # 최대 Hp
-    Hp = MaxHp  # 현재 HP
     Power = 0.0  # 공격력
+    attackCircle = 10 # 적 탐색 범위
 
-    # BulletSpeed = 1.0  # 투사채 속도
-    # BulletRange = 1.0  # 투사채 크기
-    # BulletNum = 1  # 추가 투사체 수
+    BulletSpeed = 1.0  # 투사채 속도
+    BulletRange = 1.0  # 투사채 크기
+    BulletNum = 1  # 추가 투사체 수
 
-    invers = False # 캐릭터 오른쪽, 왼쪽
+
+    invers = False  # 캐릭터 오른쪽, 왼쪽
 
     def draw(self):
-        if self.invers != True:
-            if self.state == "IDLE":
-                self.frame = self.frame % 2 + 1
-                if not self.invers:
-                    self.image.clip_draw(0 + self.frame * 34, 616 - 80, 34, 33, self.x, self.y)
-                else :
-                    self.image.clip_draw(34 * self.frame - self.frame * 34, 616 - 80, 34, 33, self.x, self.y)
+        if self.state == "IDLE":
+            self.frame = self.frame % 2 + 1
+            if not self.invers:
+                self.image.clip_draw(0 + self.frame * 34, 616 - 80, 34, 33, self.x, self.y, self.width, self.height)
+            else:
+                self.image.clip_draw(0 + self.frame * 34, 616 - 40, 34, 33, self.x, self.y, self.width, self.height)
 
-            elif self.state == "RUN":
-                self.frame = self.frame % 7 + 1
-                self.image.clip_draw(112 + self.frame * 34, 616 - 80, 34, 33, self.x, self.y)
+        elif self.state == "RUN":
+            self.frame = self.frame % 6 + 1
+            if not self.invers:
+                self.image.clip_draw(114 + self.frame * 33, 616 - 80, 33, 33, self.x, self.y, self.width, self.height)
+            else:
+                self.image.clip_draw(114 + self.frame * 34, 616 - 40, 34, 33, self.x, self.y, self.width, self.height)
+    def Move(self, MapEndLeft=0, MapEndRight=1280, MapEndBottom=720, MapEndTop=0):
+        if self.x > MapEndLeft and self.x < MapEndRight:
+            self.x += self.x_dir * self.speed
+        elif self.x < MapEndRight - 20:
+            self.x = 20
+        elif self.x > MapEndLeft + 20:
+            self.x = 1260
+
+        if self.y < MapEndBottom and self.y > MapEndTop:
+            self.y += self.y_dir * self.speed
+        elif self.y < MapEndBottom - 20:
+            self.y = 20
+        elif self.y > MapEndTop + 20:
+            self.y = 700
+
+        pass
+class Enemy:
+    frame = 0
+    state = None
+    name = None
+    image = None
+    x = 0
+    y = 0
+    width = 0
+    height = 0
+
+    x_dir = 0
+    y_dir = 0
+
+    speed = 0  # 이동속도
+
+    MaxHp = 10.0  # 최대 Hp
+    power = 0  # 공격력
+    invers = False # 캐릭터 오른쪽, 왼쪽
+
+    def __init__(self):
+        if(self.name == "Waddle_dee"):
+            self.MaxHp = 10
+            self.speed = 0.5
+            self.width = 24
+            self.height = 24
+            self.power = 2
+        elif(self.name == "knight"):
+
+            pass
+
+        self.Hp = self.MaxHp
+
+
+
+
+    def Move(self, MapEndLeft = 0, MapEndRight = 1280, MapEndBottom = 720, MapEndTop = 0):
+        self.x += self.x_dir * self.speed
+        self.y += self.y_dir * self.speed
+    def draw(self, image):
+        self.frame = self.frame%4 + 1
+        if self.name == "Waddle_dee":
+            if not self.invers:
+                image.clip_draw(40 * self.frame,1190 - 24 , 24, 24,self.x,self.y)
+            else :
+                image.clip_draw(40 * self.frame,1190 - 50 , 24, 24,self.x,self.y)
+                pass
+            pass
         pass
 
-    def chase(self, player):
-        if self.x > player.x:
+    def Attack(self, player_left, player_right, player_top, player_bottom, player_Hp):
+        pass
+    def chase(self, player_x, player_y):
+        if self.x > player_x:
+            self.invers = True
             self.x_dir = -1
         else :
+            self.invers = False
             self.x_dir = +1
 
-        if self.y > player.y:
+        if self.y > player_y:
             self.y_dir = -1
         else:
             self.y_dir = 1
 
-        pass
-    def Move(self, MapEndLeft = 0, MapEndRight = 1280, MapEndBottom = 720, MapEndTop = 0):
-        if self.x > MapEndLeft and self.x < MapEndRight :
-            self.x += self.x_dir * self.speed
-
-        if self.y < MapEndBottom and self.y > MapEndTop :
-            self.y += self.y_dir * self.speed
-        pass
--- play_state.py
+++ play_state.py
@@ -1,30 +1,33 @@
 from pico2d import *
 import game_framework
+import random
 from Character import Player
 from Character import Enemy
-
-class rect:
-    left = 0
-    right = 0
-    bottom = 0
-    top = 0
-    pass
 
 max_col = 40
 max_row = 40
 
 kirby = Player()
-
-Enemys = [Enemy in range(0,10)]
-
-# tiles = [[rect for j in range(max_col)] for i in range(max_row)]
+Enemys = [Enemy() for i in range(0,10)]
+enemy_image = None
 BG_tile_image = None
+UI_image = None
+Timer = 0
+
 def enter():
-    global BG_tile_image, tiles, kirby
+    global BG_tile_image, tiles, kirby, Enemys, enemy_image, UI_image
     BG_tile_image = load_image("assets/img/tilesets/ForestTexturePacked.png")
+    UI_image = load_image("assets/Ui/UI.png")
     kirby.image = load_image("assets/img/Kirby/Ice_Kirby_empty.png")
+    enemy_image= load_image("assets/img/Enemy/Normal_Enemy.png")
+    kirby.type = "ICE"
 
-
+    for s_Enemy in Enemys :
+        s_Enemy.name = "Waddle_dee"
+        s_Enemy.x = random.randint(0, 1280)
+        s_Enemy.y = random.randint(0, 720)
+        s_Enemy.__init__()
+        pass
 def exit():
     global BG_tile_image
     del BG_tile_image
@@ -32,16 +35,48 @@ def exit():
 
 def update():
     kirby.Move()
+    if kirby.invisivleTime > 0 :
+        kirby.invisivleTime -= 0.03
+
+    for s_Enemy in Enemys :
+        s_Enemy.chase(kirby.x , kirby.y)
+        s_Enemy.Move()
+        kirby.check_Enemy_Coll(s_Enemy.x - s_Enemy.width//2,
+                               s_Enemy.x + s_Enemy.width//2,
+                               s_Enemy.y + s_Enemy.height//2,
+                               s_Enemy.y - s_Enemy.height//2,
+                               s_Enemy.power)
+
+
     pass
 
 def draw():
+    global Timer
     clear_canvas()
     for col in range(0, max_col):
         for row in range(0, max_row):
-            BG_tile_image.clip_draw(205,206,102,102,102 * col, 102 * row)
+            BG_tile_image.clip_draw(205,206,102,102, 102 * col, 102 * row, 110,110)
+        pass
+
+    # 경험치 출력
+    UI_image.clip_draw(374, 512-249 -23, 10,23, 1280//2,700, 1280,40)
+
+    for s_Enemy in Enemys:
+        s_Enemy.draw(enemy_image)
         pass
     kirby.draw()
+    # 체력 출력
+    UI_image.clip_draw(280, 512-158 -9, 9,9, kirby.x, kirby.y - 30, 60,8)
+    UI_image.clip_draw(422, 512-158 -9, 9,9, kirby.x, kirby.y - 30, kirby.Hp/kirby.MaxHp * 60,8)
+
+    # 아이템 출력
+    UI_image.clip_draw(175,512-98 - 32, 96,32 , 100, 720 - 75, 200, 200//3)
+
+
     update_canvas()
+
+    Timer += 0.03
+    delay(0.03)
     # fill here
     pass
 
